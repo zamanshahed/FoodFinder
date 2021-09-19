0a585bf5dadf8aa37cfc15ed662b53f74d9d9187
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-09-19 23:55:04 +0600

    logical operator checked

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [uuid, setUuid] = useState("0");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {uuid === "2" && (
+        <TouchableOpacity
+          onPress={() => {
+            setUuid("0");
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Booted on to Main Thread: tap to turn off!
+          </Text>
+        </TouchableOpacity>
+      )}
+
+      {uuid === "1" && (
+        <TouchableOpacity
+          onPress={() => {
+            setUuid("2");
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Boot success: tap to open Main Thread !
+          </Text>
+        </TouchableOpacity>
+      )}
+
+      {uuid === "0" && (
+        <TouchableOpacity
+          onPress={() => {
+            setUuid("1");
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Subject turned off: tap to boot again !
+          </Text>
+        </TouchableOpacity>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
