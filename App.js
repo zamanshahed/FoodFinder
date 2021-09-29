99639450193b46d4755e1792068f44df00197e8c
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-09-29 10:56:13 +0600

    bottom tab navigation added with demo screens

--- App.js
+++ App.js
@@ -5,10 +5,31 @@ import {
   Comfortaa_400Regular,
   Aldrich_400Regular,
 } from "@expo-google-fonts/dev";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AppLoading from "expo-app-loading";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { Text } from "react-native";
+
+const Settings = () => {
+  return <Text>Settings Screen !</Text>;
+};
+const Maps = () => {
+  return <Text>Maps Here !</Text>;
+};
+
+const Tab = createBottomTabNavigator();
+
+function MainBottomTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Maps" component={Maps} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -22,7 +43,10 @@ export default function App() {
   } else {
     return (
       <>
-        <RestaurantsScreen />
+        {/* <RestaurantsScreen /> */}
+        <NavigationContainer>
+          <MainBottomTab />
+        </NavigationContainer>
       </>
     );
   }
